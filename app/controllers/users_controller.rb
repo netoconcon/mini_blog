812d4bdcf746a5b76e0bf48ed9a2c901fa0e14a7
Neto Concon <concon.neto2@gmail.com>
2018-11-03 16:53:02 -0300

    add delete button

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,12 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    @user.save
+    redirect_to user_path(@user)
   end
 
   def index
@@ -10,14 +14,31 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to users_path
+    else
+      render :new
+    end
   end
 
   def destroy
+    user = User.find(params[:id])
+    user.destroy
+    redirect_to users_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :github, :job)
   end
 end
