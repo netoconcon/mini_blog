bd3776f0e5625b95f656d3f47e369c5f71976df2
Neto Concon <concon.neto2@gmail.com>
2018-11-03 15:22:19 -0300

    add index action and view

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def index
+    @users = User.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_many :posts, dependent: :destroy
+  validates :name, presence: true
+  validates :github, presence: true
+  validates :job, presence: true
 end
